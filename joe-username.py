d7bb5333e97f92ba192f06b70264e21e17c82ad0
Vyom Jain <vyom.j@protonmail.com>
2020-10-30 22:48:06 +0530

    Create joe-username.py

---
+++ joe-username.py
@@ -0,0 +1,11 @@
+import random
+
+JOES_CONSTANTS = ["right", "awesome", "interesting", "cool","cool"]
+def generate(max_limit=4):
+  max_limit = int(max_limit)
+  word = ""
+  for i in range(max_limit):
+    rand_word = random.choice(JOES_CONSTANTS)
+    word += rand_word[0].upper() + rand_word[1:]
+  return word
+print(generate(random.randint(1,4)))
