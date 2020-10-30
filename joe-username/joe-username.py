676e502eb3ce926d7cb6079fe61ac4b066e36d73
Vyom Jain <vyom.j@protonmail.com>
2020-10-31 01:36:06 +0530

    Update joe-username.py

--- joe-username/joe-username.py
+++ joe-username/joe-username.py
@@ -10,10 +10,10 @@ def generate(lim:int=4):
 
 with open("README.md") as f:
   fl = f.readlines()
-  i1 = f.index('<!--username:START-->\n')
-  i2 = f.index('<!--username:END-->\n')
+  i1 = fl.index('<!--username:START-->\n')
+  i2 = fl.index('<!--username:END-->\n')
   
   user_name = generate(random.randint(3,11)) + '\n'
-  fl = f[:i1+1] + [user_name] + f[i2:]
+  fl = fl[:i1+1] + [user_name] + fl[i2:]
   print(fl)
 # open("README.md",'w').write(out)
