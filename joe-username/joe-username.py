c37a1b84975bf7ac31401747131aaf5197c0e735
Vyom Jain <vyom.j@protonmail.com>
2020-10-31 00:54:40 +0530

    Update joe-username.py

--- joe-username/joe-username.py
+++ joe-username/joe-username.py
@@ -5,6 +5,6 @@ def generate(lim:int=4):
   word = ""
   for i in range(lim):
     rand_word = random.choice(JOE_WORDS)
-    word += rand_word.capitalise()
+    word += rand_word.title()
   return word
 print(generate(random.randint(3,11)))
