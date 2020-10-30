1898f6a06dcc767e214cf05c4a20ba9f226b1742
Vyom Jain <vyom.j@protonmail.com>
2020-10-31 01:12:57 +0530

    Update joe-username.py

--- joe-username/joe-username.py
+++ joe-username/joe-username.py
@@ -8,3 +8,8 @@ def generate(lim:int=4):
     word += rand_word.title()
   return word
 print(generate(random.randint(3,11)))
+
+
+with open("README.md") as f:
+  print(f)
+# open("README.md",'w').write(out)
